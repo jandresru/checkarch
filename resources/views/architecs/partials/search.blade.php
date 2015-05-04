d464753f67b27e5529afa0550abc5101bcbaed62
jandresru <juli@tenactic.com>
2015-05-04 21:04:14 +0200

    v 0.1

--- resources/views/architecs/partials/search.blade.php
+++ resources/views/architecs/partials/search.blade.php
@@ -1,22 +1,20 @@
-					<p>Where?</p>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox"> Europe
-						</label>
-					</div>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox"> Asia
-						</label>
-					</div>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox"> Mordor
-						</label>
-					</div>
-					<div class="checkbox">
-						<label>
-							<input type="checkbox"> Hospitalet de Llobregat
-						</label>
-					</div>
+					<h4 class="color">Location?</h4>
+					<div class="checkbox"><label><input type="checkbox"> Belgium</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Croatia</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Denmark</label></div>
+					<div class="checkbox"><label><input type="checkbox"> France</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Germany</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Hungary</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Italy</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Netherlands</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Portugal</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Russia</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Spain</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Turkey</label></div>
+					<div class="checkbox"><label><input type="checkbox"> United Kingdom</label></div>
+					<h4 class="color">Type?</h4>
+					<div class="checkbox"><label><input type="checkbox"> Type A</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Type B</label></div>
+					<div class="checkbox"><label><input type="checkbox"> Type C</label></div>
+
 					<input type="submit" class="btn btn-primary" value="Search"/>
